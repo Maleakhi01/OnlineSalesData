1e4bfaae5da5dcbc56a52b686d3f6f447ad6fd0a
Maleakhi01 <129987108+Maleakhi01@users.noreply.github.com>
2024-06-03 17:32:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ st.title("Online Sales Data Dashboard")
 file_path = "Online Sales Data.csv"
 df = pd.read_csv(file_path)
 
+df['Date'] = pd.to_datetime(df['Date'])
+
+# Extract month from the 'Date' column and create a new 'Month' column
+df['Month'] = df['Date'].dt.month
+df.drop(columns=['Date'], inplace=True)
+
 st.sidebar.header("Please Filter Here:")
 
 product_category = st.sidebar.multiselect(
@@ -32,38 +38,87 @@ payment_method = st.sidebar.multiselect(
     default=df["Payment Method"].unique()
 )
 
-df_selection = df.query(
-    "`Product Category` == @product_category & Region == @region & `Payment Method` == @payment_method"
+month = st.sidebar.multiselect(
+    "Select the Month:",
+    options= df["Month"].unique(),
+    default= df["Month"].unique()
 )
+
+df_selection = df.query(
+    "`Product Category` == @product_category & Region == @region & `Payment Method` == @payment_method & Month == @month"
+)
+
 
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
     if "Total Revenue" in df_selection.columns:
         total_revenue = int(df_selection["Total Revenue"].sum())
-        st.subheader(f"Total Revenue")
-        st.subheader(f"${total_revenue}")
+        st.markdown(
+            f"""
+            <div style='
+                padding: 20px;
+                background-color: #f0f0f0;
+                border-radius: 5px;
+                box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.1);
+            '>
+                <h3 style='text-align: center;'>Total Revenue</h3>
+                <h2 style='text-align: center;'>${total_revenue}</h2>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
     else:
         st.write("The column 'Total Revenue' does not exist in the data.")
 with middle_column:
     region_revenue = df_selection.groupby('Region')['Total Revenue'].sum().reset_index()
     top_region = region_revenue.loc[region_revenue['Total Revenue'].idxmax()]
     top_region_revenue = top_region["Region"]
-    st.subheader(f"Top Region in Revenue")
-    st.subheader(f"{top_region_revenue}")
+    st.markdown(
+            f"""
+            <div style='
+                padding: 20px;
+                background-color: #f0f0f0;
+                border-radius: 5px;
+                box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.1);
+            '>
+                <h3 style='text-align: center;'>Top Region</h3>
+                <h2 style='text-align: center;'>{top_region_revenue}</h2>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
 with right_column:
     average_transactions = round(df_selection["Total Revenue"].mean(),2)
-    st.subheader(f"Average Revenue per Transactions")
-    st.subheader(f"${average_transactions}")
+    st.markdown(
+            f"""
+            <div style='
+                padding: 20px;
+                background-color: #f0f0f0;
+                border-radius: 5px;
+                box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.1);
+            '>
+                <h3 style='text-align: center;'>Average Transactions</h3>
+                <h2 style='text-align: center;'>${average_transactions}</h2>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
 
 st.markdown("---")
 
-# st.dataframe(df_selection)
+revenue_by_month = df_selection.groupby(by=["Month"]).sum()[["Total Revenue"]].reset_index()
+st.subheader(f"Revenue by Month")
+fig = px.line(revenue_by_month, x='Month', y='Total Revenue')
+fig.update_traces(mode='markers+lines', marker=dict(size=10))
+fig.update_xaxes(title='Month', tickmode='array', tickvals=list(range(1, 13)), ticktext=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+fig.update_yaxes(title='Total Revenue')
+st.plotly_chart(fig)
 
 left_revenue, right_revenue = st.columns(2)
 
 with left_revenue:
     revenue_by_category = df_selection.groupby(by=["Product Category"]).sum()[["Total Revenue"]].sort_values(by="Total Revenue", ascending=False).reset_index()
-        
+            
     st.subheader(f"Revenue by Category")
 
     chart = alt.Chart(revenue_by_category).mark_bar(size=20).encode(
@@ -72,10 +127,21 @@ with left_revenue:
         tooltip=["Product Category", "Total Revenue"]
     ).properties(
         width=400,
-        height=alt.Step(10 * len(revenue_by_category)) 
+        height=425
     )
 
-    st.altair_chart(chart, use_container_width=True)
+    text = chart.mark_text(
+        align='center',
+        baseline='middle',
+        dx=2  # Adjust horizontal position of text labels
+    ).encode(
+        text=alt.Text("Total Revenue:Q", format=".2f"),  # Display the revenue values
+    )
+
+    # Layer text on top of the bar chart
+    chart_with_text = (chart + text)
+
+    st.altair_chart(chart_with_text, use_container_width=True)
 
 with right_revenue:
     revenue_by_region = df_selection.groupby(by=["Region"]).sum()[["Total Revenue"]].sort_values(by="Total Revenue", ascending=False).reset_index()
@@ -88,34 +154,22 @@ with right_revenue:
         tooltip=["Region", "Total Revenue"]
     ).properties(
         width=400,
-        height=alt.Step(10 * len(revenue_by_region))
+        height=425
     )
-    st.altair_chart(chart_region, use_container_width=True)
+    
+    # Define text layer
+    text = chart_region.mark_text(
+        align='center',
+        baseline='middle',
+        dx=3 
+    ).encode(
+        text=alt.Text("Total Revenue:Q", format=".2f"),  # Display the revenue values
+    )
 
-try:
-    df_selection['Date'] = pd.to_datetime(df_selection['Date'])
-except Exception as e:
-    st.error(f"Error occurred while converting 'Date' column to datetime: {e}")
+    # Layer text on top of the bar chart
+    chart_with_text = (chart_region + text)
 
-try:
-    df_selection['Month'] = df_selection['Date'].dt.month
-except Exception as e:
-    st.error(f"Error occurred while extracting month from 'Date' column: {e}")
-
-try:
-    revenue_by_month = df_selection.groupby('Month')['Total Revenue'].sum().reset_index()
-except Exception as e:
-    st.error(f"Error occurred while calculating revenue by month: {e}")
-
-st.subheader(f"Revenue by Month")
-try:
-    fig = px.line(revenue_by_month, x='Month', y='Total Revenue')
-    fig.update_traces(mode='markers+lines', marker=dict(size=10))
-    fig.update_xaxes(title='Month', tickmode='array', tickvals=list(range(1, 13)), ticktext=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
-    fig.update_yaxes(title='Total Revenue')
-    st.plotly_chart(fig)
-except Exception as e:
-    st.error(f"Error occurred while plotting line chart: {e}")
+    st.altair_chart(chart_with_text, use_container_width=True)
 
 
 left_transactions, right_transactions = st.columns(2)
@@ -125,16 +179,12 @@ with left_transactions:
     transaction_counts = df_selection['Payment Method'].value_counts().reset_index()
     transaction_counts.columns = ['Payment Method', 'Transaction Count']
 
-    chart_transaction_method = alt.Chart(transaction_counts).mark_bar(size=20).encode(
-        x='Transaction Count',
-        y=alt.Y('Payment Method', sort='-x'),
-        tooltip=['Payment Method', 'Transaction Count']
-    ).properties(
-        width=600,
-        height=alt.Step(10*len(transaction_counts))
-    )
-
-    st.altair_chart(chart_transaction_method, use_container_width=True)
+    try:
+        fig = go.Figure(data=[go.Pie(labels=transaction_counts['Payment Method'], 
+                                    values=transaction_counts['Transaction Count'])])
+        st.plotly_chart(fig)
+    except Exception as e:
+        st.error(f"Error occurred: {e}")
 
 with right_transactions:
     st.subheader(f"Transactions by Product Category")
@@ -147,3 +197,17 @@ with right_transactions:
         st.plotly_chart(fig)
     except Exception as e:
         st.error(f"Error occurred: {e}")
+
+
+top_10_left, top_10_right = st.columns(2)
+
+with top_10_left:
+    st.subheader(f"Top 10 Products by Units Sold")
+    top_products = df_selection.groupby("Product Name").sum()["Units Sold"].sort_values(ascending=False).head(10)
+    st.write(top_products)
+
+with top_10_right:
+    st.subheader(f"Top 10 Products by Revenue")
+    top_products_revenue = df_selection.groupby("Product Name").sum()["Total Revenue"].sort_values(ascending=False).head(10)
+    st.write(top_products_revenue)
+
